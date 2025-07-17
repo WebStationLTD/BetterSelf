8aee71f0d3cbf2e727fb99b4d12776d86d605ad0
7onc1 <anton.demirev84@gmail.com>
2025-07-17 20:05:06 +0300

    Update sponsor PDF download button text
    Changed the button label from 'Изтеглете PDF за повече информация' to 'Изтегли спонсорски пакети тук' for improved clarity.

--- components/SponsorsAndPartners.js
+++ components/SponsorsAndPartners.js
@@ -112,7 +112,7 @@ export default function SponsorsAndPartners() {
               rel="noopener noreferrer"
               className="rounded-md bg-[#ff8d00] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
             >
-              Изтеглете PDF за повече информация
+              Изтегли спонсорски пакети тук
             </Link>
           </div>
         </div>
